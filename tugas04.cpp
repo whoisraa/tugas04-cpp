17d3a112dec8659e186c230e8d03edd47f1dc15b
whoisraa <rairaiatlantica@gmail.com>
2021-06-26 14:59:21 +0700

    tugas 4 cpp

---
+++ tugas04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x;
+	x = 30;
+	cout<<"Nilai Awal X : ";
+	cout<<x<<endl;
+	x--;
+	cout<<"Nilai Kedua X : ";
+	cout<<x<<endl;
+	x++;
+	x++;
+	cout<<"Nilai Ketiga X : ";
+	cout<<x;
+}
